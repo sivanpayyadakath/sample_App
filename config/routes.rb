1df399666350a59ebb3d4ea8ddf466f188502f3f
sivanpayyadakath <sivan.payyadakath@gmail.com>
2018-11-07 17:37:04 +0900

    implemented login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -15,7 +17,14 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
 
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
